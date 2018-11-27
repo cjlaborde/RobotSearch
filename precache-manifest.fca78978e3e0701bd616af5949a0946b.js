bd7065fe0b9c5ac954b370bb3f416e9e619d1731
cjlaborde <cjlaborde@gmail.com>
2018-11-27 16:10:37 -0400

    Updates

---
+++ precache-manifest.fca78978e3e0701bd616af5949a0946b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotSearch/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "baebbd56ef58147430d0",
+    "url": "/RobotSearch/static/js/runtime~main.baebbd56.js"
+  },
+  {
+    "revision": "b535dfc52dfd347a2b34",
+    "url": "/RobotSearch/static/js/main.b535dfc5.chunk.js"
+  },
+  {
+    "revision": "9cbd6d32e1daf617507e",
+    "url": "/RobotSearch/static/js/1.9cbd6d32.chunk.js"
+  },
+  {
+    "revision": "b535dfc52dfd347a2b34",
+    "url": "/RobotSearch/static/css/main.3e54df5e.chunk.css"
+  },
+  {
+    "revision": "9cbd6d32e1daf617507e",
+    "url": "/RobotSearch/static/css/1.4d46da2b.chunk.css"
+  },
+  {
+    "revision": "128a2988f873afb07dbe9b879db309ac",
+    "url": "/RobotSearch/index.html"
+  }
+];
